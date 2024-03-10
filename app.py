14fe4881af25e306225091152742d14fa9eb18a7
geetesh29 <geeteshramteke291996@gmail.com>
2024-03-10 13:45:37 +0530

    flask API added

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 # this is python module
+import numpy as np
+import pandas as pd 
+
+from flask import flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Default API"
+
+if __name__=="__main__":
+    app.run(debug=True)
